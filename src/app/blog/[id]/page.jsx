a2d35d0327e13ecdd73c792b66365aa0a6dadfd2
marcelopaivag <mff.paiva@gmail.com>
2024-04-26 10:07:58 -0400

    Blog funcional

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,10 +1,19 @@
-import { posts } from '@/app/Posts'; 
-import BlogArticle from '@/components/BlogArticle'
+'use client'
 
-const page = () => {
-  return (
-    <BlogArticle posts={posts} />
-  )
-}
+import { useParams } from 'next/navigation';
+import { posts } from '@/app/Posts'; // Asegúrate de importar tus posts desde donde los tengas definidos
+import BlogArticle from '@/components/BlogArticle';
 
-export default page
+const BlogPostPage = () => {
+  const { id } = useParams(); // Obtener la ID del post de la URL
+  const post = posts.find(post => post.id === parseInt(id)); // Encontrar el post correspondiente
+
+  if (!post) {
+    return <div>Post no encontrado</div>; // Manejar el caso en que el post no se encuentre
+  }
+
+  return <BlogArticle post={post} />; // Renderizar el componente BlogPost con el post obtenido
+};
+
+export default BlogPostPage;
+
